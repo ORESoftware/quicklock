db3debe559e049cbefa240a79dd3c63760acc93d
Olegzandr VD <alex@oresoftware.com>
2018-03-01 20:43:58 -0800

    pdev:set

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 set -e;
+
 cd "$HOME"
+
 mkdir -p "$HOME/.quicklock/locks"
-curl https://rawgit.com/oresoftware/quicklock/master/install.sh > "$HOME/.quicklock/ql.sh"
+
+curl https://rawgit.com/oresoftware/quicklock/master/install.sh --output "$HOME/.quicklock/ql.sh"
 
 echo "To complete installation of 'quicklock' add the following line to your .bash_profile file:";
+
 echo ". \"$HOME/.quicklock/ql.sh\"";
+
