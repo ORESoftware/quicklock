1f58d133778e085d6f49b81062416bdf4bbf2c1a
Olegzandr VD <alex@oresoftware.com>
2018-03-02 17:27:34 -0800

    pdev:set

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-os=$(uname -s)
+set -e;
+
+os="$(uname -s)";
 
 if [[ "$os" == "Darwin" ]]; then
     gcc src/kqueue_darwin.c -o bin/kqueue_darwin
@@ -8,4 +10,4 @@ else
     gcc src/linux_waiter.c -o bin/linux_waiter
 fi
 
-chmod u+x lib/*
+chmod u+x bin/*  || echo "quicklock: warning: user may need root privileges to make bin/* files executable.";
