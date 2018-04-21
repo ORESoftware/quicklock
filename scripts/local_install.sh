087603c7067177bccdf8bc0b6aaeff8cf2826834
Olegzandr VD <alex@oresoftware.com>
2018-04-20 20:04:06 -0700

    pdev:set

--- scripts/local_install.sh
+++ scripts/local_install.sh
@@ -2,12 +2,12 @@
 
 set -e;
 
-cat ql.sh > "$HOME/.quicklock/ql.sh"
-source "$HOME/.quicklock/ql.sh"
+cat ql.sh > "$HOME/.quicklock/ql.sh";
+source "$HOME/.quicklock/ql.sh";
 
 npm link  #  link quicklock lib
 
 (
   cd "$HOME/.quicklock/nodejs";
-  npm link quicklock
+  npm link quicklock;
 )
